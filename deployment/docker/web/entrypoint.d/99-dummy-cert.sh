378e06930857baa93c008e4d1da3b65f622d407e
Christian Mahnke <cmahnke@gmail.com>
2022-01-26 19:37:34 +0100

    don't fail if not configured properly

--- deployment/docker/web/entrypoint.d/99-dummy-cert.sh
+++ deployment/docker/web/entrypoint.d/99-dummy-cert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p /etc/nginx/ssl/live/${NGINX_HOST}
-cp /etc/nginx/ssl/live/localhost/fullchain.pem /etc/nginx/ssl/live/${NGINX_HOST}/fullchain.pem
-cp /etc/nginx/ssl/live/localhost/privkey.pem /etc/nginx/ssl/live/${NGINX_HOST}/privkey.pem
+cp -u /etc/nginx/ssl/live/localhost/fullchain.pem /etc/nginx/ssl/live/${NGINX_HOST}/fullchain.pem
+cp -u /etc/nginx/ssl/live/localhost/privkey.pem /etc/nginx/ssl/live/${NGINX_HOST}/privkey.pem
